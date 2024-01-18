bf67a42e479b04389d1b69db6585fd31ff4ac111
uarteaga <ulisesarteaga10@gmil.com>
2024-01-17 20:51:27 -0600

    Tipos de datos, números, funciones aritmeticas, y funciones, en python

---
+++ 003.arithmetic.py
@@ -0,0 +1,25 @@
+'''
+003. arithmetic
++ suma
+- resta
+* multiplicación
+/ división
+
+'''
+
+print (2+2) #suma
+print (5-1) #resta
+print (5*3) #multiplicación
+print (10/2) #división
+
+# Módulo '%' arroja el residuo de la multiplicación del número de la derecha por si mismo, contra el número de la izquierda
+print (5%2)#1
+print (9%3)#0
+print (10%3)#1
+
+#exponentes '**'
+print (3 ** 2) #9
+print (2 ** 2) #4
+print (5 ** 3) #125
+
+print (10//3)
