2fcda53bd12291b57400e9240284d80919de18ea
uarteaga <ulisesarteaga10@gmil.com>
2024-01-22 20:49:10 -0600

    Aprendiendo listas y sus métodos

---
+++ 012224.001.list.py
@@ -0,0 +1,39 @@
+'''
+1. List
+
+'''
+
+list_my_students = [
+'Ulises', #0
+'Maggie', #1
+'Paulina', #2
+'Octavio', #3
+'Ibrahim' #4
+]
+
+'''
+print (list_my_students)
+print (list_my_students[0])
+print (list_my_students[1])
+print (list_my_students[2])
+print (list_my_students[3])
+print (list_my_students[4])
+list_my_students.append("Luis")
+list_my_students.insert(0, "Isela")
+list_my_students.extend(['Javier','Isela','Limón'])
+new_students_list = list_my_students.extend(['Javier','Isela','Limón'])
+list_my_students.pop()
+list_my_students.pop(1)
+list_my_students.remove("Ulises")
+list_my_students.clear()
+'''
+
+
+print (list_my_students)
+#remove elements
+
+print('____________')
+print (list_my_students)
+
+for item in list_my_students:
+    print (item)
