ada49a7819aa87f4d1b579619df4ba5d78cb4acf
uarteaga <ulisesarteaga10@gmil.com>
2024-02-20 19:31:21 -0600

    trabajando con venv

---
+++ 021924/Debug/001.debug_manual.py
@@ -0,0 +1,7 @@
+def double (numero):
+    print('numero: ', numero)
+    resultado = numero * 2
+    print('resultado: ', resultado)
+    return resultado
+
+print(double(2))
---
+++ 021924/Debug/002.debug_modulo.py
@@ -0,0 +1,10 @@
+import pdb
+
+def double(numero):
+    breakpoint()
+    resultado = numero * 2
+    return resultado
+
+resultado = double(2)
+
+print(resultado)
